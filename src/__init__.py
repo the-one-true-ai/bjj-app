77e0bdb99b245b697213f080da9768d6351ef62b
Achal Inamdar <achalinamdar@hotmail.com>
2025-03-12 15:17:13 +0000

    pydantic settigns and lifespan

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from src.users.routes import user_router
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"Server is starting ...")
+    yield
+    print(f"Server has been stopped.")
+
 
 version = "v1"
+
 
 app = FastAPI(
     title="BJJ App",
     description="A REST API for a sick BJJ App",
-    version=version
+    version=version,
+    lifespan=life_span
 )
+
 
 app.include_router(router = user_router, prefix=f"/api/{version}/users", tags=["users"])
---
+++ src/db/main.py
@@ -0,0 +1,9 @@
+from sqlmodel import create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from config import Config
+
+engine = AsyncEngine(
+    create_engine(
+    url=Config.DATABASE_URL,
+    echo=True
+    ))
