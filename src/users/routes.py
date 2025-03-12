9185acabefe91bb3a793124f8fe59b4239cef003
Achal Inamdar <achalinamdar@hotmail.com>
2025-03-12 17:40:15 +0000

    error handling

--- src/users/routes.py
+++ src/users/routes.py
@@ -26,26 +26,27 @@ async def create_a_user(user_data: UserCreateModel, session:AsyncSession = Depen
 async def get_a_user(user_uid:str, session:AsyncSession = Depends(get_session)):
     user = await user_service.get_a_user(user_uid, session)
     
-    if user:
-        return user
-    else:
+    if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Unable to find a user with ID:{user_uid}.")
+    else:
+        return user
 
 
 @user_router.patch("/{user_uid}", response_model=User, status_code=status.HTTP_200_OK)
 async def update_a_user(user_uid:str, user_update_data: UserUpdateModel, session:AsyncSession = Depends(get_session)) -> dict:
     updated_book = await user_service.update_a_user(user_uid, user_update_data, session)
 
-    if updated_book:
-        return updated_book
-    else:        
+    if updated_book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Unable to find a user with ID:{user_uid} to update.")        
+    else:        
+        return updated_book
 
 @user_router.delete("/{user_uid}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_a_user(user_uid:str,session:AsyncSession = Depends(get_session)):
     user_to_delete = await user_service.delete_a_user(user_uid, session)
 
-    if user_to_delete:
-        return user_to_delete
-    else:
+    if user_to_delete is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Unable to find a user with ID:{user_uid} to delete.")            
+    else:
+        return {}
+
