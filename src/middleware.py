bd54d8bfed70bdc72b071c6aac1c36e4d5db2f44
Achal Inamdar <achalinamdar@hotmail.com>
2025-03-19 14:13:59 +0000

    middleware

--- src/middleware.py
+++ src/middleware.py
@@ -34,5 +34,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["localhost", "127.0.0.1"],
+        allowed_hosts=["localhost", "127.0.0.1", "sick-bjj-app.onrender.com"],
     )
