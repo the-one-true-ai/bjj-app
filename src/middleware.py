53e5da54996a248a90768a00d76b8342d4dbdc07
Achal Inamdar <achalinamdar@hotmail.com>
2025-03-23 10:14:58 +0000

    CORS

--- src/middleware.py
+++ src/middleware.py
@@ -26,7 +26,7 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["sick-bjj-app.onrender.com/","localhost", "127.0.0.1", "localhost:5173", "localhost:8000"],
         allow_methods=["*"],
         allow_headers=["*"],
         allow_credentials=True,
