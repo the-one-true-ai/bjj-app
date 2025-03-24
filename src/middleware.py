360acf2221a886b647f2a8baeb731dab9884fb4d
Achal Inamdar <achalinamdar@hotmail.com>
2025-03-24 16:56:12 +0000

    limited origins"

--- src/middleware.py
+++ src/middleware.py
@@ -26,7 +26,7 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["localhost:5173","bjj-app-frontend.vercel.app", "localhost:8000"],
         allow_methods=["*"],
         allow_headers=["*"],
         allow_credentials=True,
