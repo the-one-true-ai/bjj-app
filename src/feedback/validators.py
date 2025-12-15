b2924562c870c9321a6931623d8a44703d4a5191
Achal Inamdar <achalinamdar@hotmail.com>
2025-12-15 13:04:12 +0000

    redis fix, api call fix, validator notes

--- src/feedback/validators.py
+++ src/feedback/validators.py
@@ -1,10 +1,20 @@
 from enum import Enum
 
+
 class MessageType(str, Enum):
     TEXT = "Text"
     AUDIO = "Audio"
     VIDEO = "Video"
 
-class FeedbackStatus(str, Enum): #TODO: Review this, doesnt really make that much sense
+
+class FeedbackStatus(
+    str, Enum
+):  # TODO: Review this, doesnt really make that much sense
     AWAITING_FEEDBACK = "Awaiting feedback"
     FEEDBACK_GIVEN = "Feedback given"
+
+
+# 1. req sent
+# 2. req accept by coach / awaiting feedback
+# 4. feedback complete - responded
+# 4. feedbcak complete - denied
