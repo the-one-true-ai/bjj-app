6fbd8289ebdf22275e90b01ea12eb3574d2a5e0c
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-12-23 10:02:10 +0000

    cloudflare-r2-bucket

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from src.auth.routes import auth_router
 from src.users.routes import user_router
 from src.feedback.routes import feedback_router
 from .errors import register_all_errors
 from .middleware import register_middleware
-
+from src.videos.routes import video_router
 
 version = "v1"
 
@@ -13,6 +15,7 @@ A REST API for the BJJ app
     """
 
 version_prefix =f"/api/{version}"
+
 
 app = FastAPI(
     title="BJJ app",
@@ -37,3 +40,4 @@ register_all_errors(app)
 app.include_router(auth_router, prefix=f"{version_prefix}/auth", tags=["auth"])
 app.include_router(user_router, prefix=f"{version_prefix}/users", tags=["users"])
 app.include_router(feedback_router, prefix=f"{version_prefix}/sessions", tags=["feedback sessions"])
+app.include_router(video_router, prefix=f"{version_prefix}/videos", tags=["videos"])
